e183b54e3750cb5603b720700c7afb3a6ebd4aa6
michael donchenko <michaeldonchenko@michaels-MacBook-Pro.local>
2024-01-15 17:07:52 +0200

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -13,18 +13,20 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Vite - Svelte</h1>
 
   <div class="card">
     <Counter />
   </div>
+
+  <p>Changesssss</p>
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
   <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    Click on the Vite and Svelte logos to learn more !@!@!@!
   </p>
 </main>
 
